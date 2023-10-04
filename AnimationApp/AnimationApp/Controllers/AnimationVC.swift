c687114edb1bcd6c0581dc36b4e19c848bf84532
Ilya Pugachev <ilyapugachv@gmail.com>
2023-10-04 12:49:10 +0300

    Native animation is done

---
+++ AnimationApp/AnimationApp/Controllers/AnimationVC.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+final class AnimationVC: UIViewController {
+
+    @IBOutlet private weak var coreAnimationView: UIView!
+    @IBOutlet private weak var stackView: UIStackView!
+    
+    @IBOutlet private weak var coreActionButton: UIButton!
+    @IBOutlet private weak var lottieAnimationButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCoreAnimationVeiw()
+    }
+
+    @IBAction func coreAnimationBtnAction(_ sender: UIButton) {
+        sender.pulsate()
+    }
+    
+    private func setupCoreAnimationVeiw() {
+        UIView.animate(withDuration: 1, delay: 0, options: [.autoreverse, .repeat]) { [weak self] in
+            self?.coreAnimationView.frame.origin.x += 40
+        }
+    }
+}
---
+++ AnimationApp/AnimationApp/Extansions/UIButtonExtensions.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+extension UIButton {
+    
+    func pulsate() {
+        let pulsate = CASpringAnimation(keyPath: "transform.scale")
+        pulsate.duration = 0.6
+        pulsate.fromValue = 0.8
+        pulsate.toValue = 1.2
+        pulsate.autoreverses = true
+        pulsate.initialVelocity = 0.5
+        pulsate.damping = 1
+        
+        layer.add(pulsate, forKey: nil)
+
+    }
+}
